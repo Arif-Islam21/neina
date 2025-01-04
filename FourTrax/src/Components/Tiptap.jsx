24a4e31e2e893e3eab4d4ffab9b2ba0c37811663
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-01-05 00:06:49 +0600

    active style added

--- FourTrax/src/Components/Tiptap.jsx
+++ FourTrax/src/Components/Tiptap.jsx
@@ -3,7 +3,7 @@ import "./styles.css";
 import { Color } from "@tiptap/extension-color";
 import ListItem from "@tiptap/extension-list-item";
 import TextStyle from "@tiptap/extension-text-style";
-import { EditorProvider } from "@tiptap/react";
+import { EditorContent, EditorProvider } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import MenuBar from "./MenuBar";
 
@@ -13,17 +13,17 @@ const extensions = [
   StarterKit.configure({
     bulletList: {
       keepMarks: true,
-      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+      keepAttributes: false,
     },
     orderedList: {
       keepMarks: true,
-      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+      keepAttributes: false,
     },
   }),
 ];
 
 const content = `
-<div>
+<div style="border: 2px solid #8b0000; padding: 10px; border-radius: 5px;">
   <h2>
     Hello! Write anything you want
   </h2>
@@ -37,7 +37,9 @@ const extent = () => {
       extensions={extensions}
       content={content}
     >
-      <div className="tiptap-editor"></div>
+      <div className="tiptap-editor">
+        <EditorContent />
+      </div>
     </EditorProvider>
   );
 };
