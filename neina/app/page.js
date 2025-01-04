b0a8075860c1ff3adef1c22629f2c393bbc56ce6
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-01-04 10:42:13 +0600

    project setup done

---
+++ neina/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Banner from "./components/Banner";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Hello Neina world</h2>
+    </div>
+  );
+}
