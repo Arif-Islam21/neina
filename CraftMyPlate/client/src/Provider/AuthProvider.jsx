f75ab447603e752ac61a3d89d79993d1a2dc43cd
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-01-14 00:27:21 +0600

    ok for today

---
+++ CraftMyPlate/client/src/Provider/AuthProvider.jsx
@@ -0,0 +1,54 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import app from "../Authentication/firebase.config";
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext(null);
+
+// eslint-disable-next-line react/prop-types
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const auth = getAuth(app);
+
+  const registerUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const loginUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (currentUser) => {
+      setUser(currentUser);
+      console.log(currentUser);
+    });
+  }, [auth]);
+
+  const authInfo = {
+    registerUser,
+    loginUser,
+    logOut,
+    user,
+    loading,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
