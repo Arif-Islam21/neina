6403668f6878520d97738c576706e846f9e667c8
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-01-28 00:52:31 +0600

    chart not implemented

--- moneyyAi/client/src/Pages/Home/Home.jsx
+++ moneyyAi/client/src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Chart from "../../Components/Home/Chart";
 import Hero from "../../Components/Home/Hero";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Chart />
     </div>
   );
 };
